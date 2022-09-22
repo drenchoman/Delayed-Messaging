6348999dcbdc58a34a44f3770f7bf7ecffd55b6b
Livi <livitaufao@gmail.com>
2022-09-23 11:05:08 +1200

    delete => fruits; added => archive comp

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,18 +3,18 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useCacheUser } from '../auth0-utils'
 import { useAuth0 } from '@auth0/auth0-react'
 
-import Nav from './Nav'
-import Fruits from './Fruits'
 import { getUser } from '../api'
 import Register from './Register'
 import { useDispatch } from 'react-redux'
 import { clearLoggedInUser, updateLoggedInUser } from '../slices/user'
 
+import Nav from './Nav'
 import Contacts from './Contacts'
 import Corresponde from './Corresponde'
 import Dashboard from './Dashboard'
 import Draft from './Draft'
 import Recieved from './Recieved'
+import Archive from './Archive'
 
 function App() {
   useCacheUser()
@@ -42,13 +42,14 @@ function App() {
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Fruits />} />
+        
         <Route path="register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/corresponde" element={<Corresponde />} />
         <Route path="/recieved" element={<Recieved />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/draft" element={<Draft />} />
+        <Route path="/archive" element={<Archive />} />
       </Routes>
     </>
   )
