c56656882c2278197df1099a539ec72be2bd5cd5
Livi <livitaufao@gmail.com>
2022-09-24 18:32:40 +1200

    hide and show func - recieve

--- client/components/view/Recieved.jsx
+++ client/components/view/Recieved.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavTwo from './NavTwo'
+import styles from '../../../server/public/styles/Recieved.module.css'
 
 function Recieved() {
+  const { body, hidden, show } = styles
+  const [ hide, setHide ] = useState(hidden)
+
   const message = {
     sender: 'John',
     recipient: 'Elizabeth',
@@ -9,13 +13,25 @@ function Recieved() {
     body: 'Dear Elizabeth \n\nI am so happy to hear your news,\n\nWhat else can you tell me?\n\nRegards \nJohn ',
   }
 
+  function displayHandler() {
+    (hide != show) ? setHide(show) : setHide(hidden)
+  }
+
   return (
     <>
     <NavTwo />
-    <div>
-      <h3>From: {message.sender}</h3>
-      <h3>Subject: {message.subject}</h3>
-      <p style={{ whiteSpace: 'pre-line' }}>{message.body}</p>
+    <div className={body}>
+      <button></button>
+      <button></button>
+      <button onClick={displayHandler}>{message.sender}</button>
+      <div  className={hide}>
+        <h3>From: {message.sender}</h3>
+        <h3>Subject: {message.subject}</h3>
+        <p style={{ whiteSpace: 'pre-line' }}>{message.body}</p>
+      </div>
+      <button></button>
+      <button></button>
+      <button></button>
     </div>
     </>
   )
