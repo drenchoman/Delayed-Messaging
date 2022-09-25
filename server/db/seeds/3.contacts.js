1da7b10936f68e495c07edd9b8392f47e1cb59ff
livi-taufao <107158405+livikavesi@users.noreply.github.com>
2022-09-26 09:43:44 +1300

    Update 3.contacts.js

--- server/db/seeds/3.contacts.js
+++ server/db/seeds/3.contacts.js
@@ -6,31 +6,15 @@ exports.seed = (knex) => {
         {
           id: 1,
           user_id: 'auth0|123',
-          friend: 'auth0|456',
+          name: 'Grape Man',
+          username: 'grape_gatsby',
           blocked: false,
         },
         {
           id: 2,
-          user_id: 'auth0|123',
-          friend: 'auth0|1200',
-          blocked: false,
-        },
-        {
-          id: 3,
-          user_id: 'auth0|123',
-          friend: 'auth0|1201',
-          blocked: false,
-        },
-        {
-          id: 4,
           user_id: 'auth0|456',
-          friend: 'auth0|123',
-          blocked: false,
-        },
-        {
-          id: 5,
-          user_id: 'auth0|456',
-          friend: 'auth0|1201',
+          name: 'Banana Joe',
+          username: 'banana_llama',
           blocked: false,
         },
       ])
