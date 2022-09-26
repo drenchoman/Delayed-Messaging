49609cafb1b1e7c26346fef6e56894992770c3e3
Oscar Harron <oscarharron@hotmail.com>
2022-09-26 18:31:02 +1300

    add updated copy

--- client/components/view/Landing/Landing.jsx
+++ client/components/view/Landing/Landing.jsx
@@ -6,6 +6,7 @@ import SectionOne from './SectionOne'
 import SectionTwo from './SectionTwo'
 import SectionThree from './SectionThree'
 import SectionFour from './SectionFour'
+import SectionFive from './SectionFive'
 export default function Landing() {
   return (
     <div className={styles.wrapper}>
@@ -16,6 +17,7 @@ export default function Landing() {
         <SectionTwo />
         <SectionThree />
         <SectionFour />
+        <SectionFive />
       </div>
       <Footer />
     </div>
